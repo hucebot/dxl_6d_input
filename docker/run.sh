2a2b188feb20477d38d2c97b600a41d383727d50
Clemente Donoso <clemente.donosok@gmail.com>
2024-09-12 10:08:23 +0200

    Refactor sh command for better readability
    This change makes the command easier to read and maintain.

--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,12 @@
-docker run -it --net host --privileged -v /dev:/dev -v $HOME/catkin_ws:/catkin_ws dxl_6d:latest
+# For graphics
+xhost +local:docker
+
+docker run \
+        -it \
+        --env="DISPLAY" \
+        --env="QT_X11_NO_MITSHM=0" \
+        --net host \
+        --privileged \
+        -v /dev:/dev \
+        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+        -v $HOME/Desktop/Inria/ROS:/catkin_ws dxl_6d:latest
